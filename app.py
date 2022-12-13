eace76c9515da4df01d399af84d97d4f675cd66c
Muhammad Fadhil Akmal B. Paloloang <fadhilpaloloang10@gmail.com>
2022-12-13 16:33:02 +0800

    Zenius

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import request
 import numpy as np
 import pickle
 
-model = pickle.load(open('model/model19.pkl', 'rb'))
+model = pickle.load(open('model/modelfinalfix.pkl', 'rb'))
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -11,21 +11,34 @@ app = flask.Flask(__name__, template_folder='templates')
 def main():
     return(flask.render_template('main.html'))
 
+
+
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    Sex = int(request.form['Sex'])
-    ChestPainType = int(request.form['ChestPainType'])
-    MaxHR = int(request.form['MaxHR'])
-    ExerciseAngina = int(request.form['ExerciseAngina'])
-    Oldpeak = float(request.form['Oldpeak'])
-    ST_Slope = int(request.form['ST_Slope'])
-    predict_list = [[Sex, ChestPainType, MaxHR, ExerciseAngina, Oldpeak, ST_Slope]]
+    EXT_SOURCE_2 = float(request.form['EXT_SOURCE_2'])
+    EXT_SOURCE_3 = float(request.form['EXT_SOURCE_3'])
+    DAYS_EMPLOYED = int(request.form['DAYS_EMPLOYED'])
+    DAYS_BIRTH = float(request.form['DAYS_BIRTH'])
+    REGION_RATING_CLIENT_W_CITY = int(request.form['REGION_RATING_CLIENT_W_CITY'])
+    REGION_RATING_CLIENT = int(request.form['REGION_RATING_CLIENT'])
+    DAYS_CREDIT = int(request.form['DAYS_CREDIT'])
+    NAME_EDUCATION_TYPE = int(request.form['NAME_EDUCATION_TYPE'])
+    DAYS_LAST_PHONE_CHANGE = int(request.form['DAYS_LAST_PHONE_CHANGE'])
+    DAYS_ENDDATE_FACT = int(request.form['DAYS_ENDDATE_FACT'])
+    DAYS_CREDIT_ENDDATE = int(request.form['DAYS_CREDIT_ENDDATE'])
+    CNT_PAYMENT = int(request.form['CNT_PAYMENT'])
+    DAYS_CREDIT_UPDATE = int(request.form['DAYS_CREDIT_UPDATE'])
+    CODE_GENDER = int(request.form['CODE_GENDER'])
+    NAME_INCOME_TYPE = int(request.form['NAME_INCOME_TYPE'])
+
+    predict_list = [[EXT_SOURCE_2, EXT_SOURCE_3, DAYS_EMPLOYED, DAYS_BIRTH, REGION_RATING_CLIENT_W_CITY, REGION_RATING_CLIENT, DAYS_CREDIT, NAME_EDUCATION_TYPE, DAYS_LAST_PHONE_CHANGE, DAYS_ENDDATE_FACT, DAYS_CREDIT_ENDDATE, CNT_PAYMENT, DAYS_CREDIT_UPDATE, CODE_GENDER, NAME_INCOME_TYPE]]
+    
     prediction = model.predict(predict_list)
-    output = {0: 'Tidak Sakit', 1: 'Sakit Jantung'}
-    return flask.render_template('main.html', prediction_text='Prediksi pasien dinyatakan:  {}'.format(output[prediction[0]]))
+    output = {0: 'Lancar Bayar', 1: 'Gagal Bayar'}
+    return flask.render_template('main.html', prediction_text='Klien teridentifikasi :  {}'.format(output[prediction[0]]))
 
 if __name__ == '__main__':
     app.run(debug=True)
